c806beafd5087439fe4534b78a1fbb7edf81dc6b
Syed <amanxsyed@gmail.com>
2025-03-30 03:52:28 +0500

    Add token generation and cookie handling in loginUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.model.js";
 import { cloudinaryUpload } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessandRefreshToken = async (userId) => {
+  try{
+    const user = await User.findById(userId);
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+
+  } catch(error){ 
+    throw new ApiError(500, "Something went wrong while generating access & refresh token!");
+  }
+}
+
+
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
   //validation - not empty
@@ -94,6 +111,43 @@ const loginUser = asyncHandler(async(req, res)=>{
     $or: [{ email }, { username }]
   });
 
+  if(!user){
+    throw new ApiError(404, "User doesn't exist!");
+  }
+
+  const isPasswordValid = await user.comparePassword(password)
+
+  if(!passwordValid){
+    throw new ApiError(401, "Invalid password!");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessandRefreshToken(user._id);
+
+  const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+  const options = {
+    httpOnly: true,
+    secure : true
+  }
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+    new ApiResponse(200, {
+      user: loggedInUser,
+      accessToken,
+      refreshToken
+    }
+    , "User logged in successfully!"
+  )
+  )
+
+
+
 });
+
+
 
 export { registerUser, loginUser };
